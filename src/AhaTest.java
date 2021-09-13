460a20c14bb25a5b23a152873bbc3f7f583b2a17
Jace Cooper <cooperja85@ou.edu>
2021-09-13 14:14:22 -0500

    Made the JUnit Test

---
+++ src/AhaTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AhaTest {
+
+	@Test
+	void testAha() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString1() {
+		fail("Not yet implemented");
+	}
+
+}
